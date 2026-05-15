aa6872bae63219d84cda25f95eff59cd1ea70da5
Grigorii <grigory.bocharsky@gmail.com>
2026-05-15 19:03:33 +0300

    Fetching of available addresses added and work on creating socket started

--- main.c
+++ main.c
@@ -1,16 +1,36 @@
+#include <sys/types.h>
 #include <sys/socket.h>
+#include <netdb.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-int main(){
+int main(int argc, char *argv[]){
+    int sfd;
+    struct addrinfo hints, *res;
 
-
-    int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if(socket_fd < 0)
-    {
-        perror("socket");
-        exit(1);
+    if(argc < 2){
+        fprintf(stderr, "Usage: %s <port>\n", argv[0]);
+        exit(EXIT_FAILURE);
     }
 
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = 0;
+    hints.ai_flags = AI_PASSIVE;
+    hints.ai_addrlen = 0;
+    hints.ai_addr = NULL;
+    hints.ai_cannonname = NULL;
+    hints.ai_next = NULL;
 
+    if((getaddrinf(NULL, argv[1], &hints, &res))){
+	fprintf(stderr, "An error occured while getting addresses\n");
+	exit(EXIT_FAILURE);
+    }
+
+    for(addr=hints; addr != NULL; addr = hints->
+
+    freeaddrinfo(res);
+   
     return 0;
 }
